db0e5c032a1b7ed0e21e651c1b74ae3fa04720ac
Roozbeh Zabihollahi <ruzbeh@gmail.com>
2022-10-29 18:53:19 -0700

    remove comments and logs

--- web/controller/base.go
+++ web/controller/base.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strings"
-	"x-ui/logger"
 	"x-ui/web/session"
 )
 
@@ -13,13 +12,7 @@ type BaseController struct {
 
 func (a *BaseController) checkLogin(c *gin.Context) {
 
-	// TODO: api-lookup
-	// TODO: num-users
 	ipWhitelist := []string{"::1", "127.0.0.1", "192.184.149.170" /*R*/, "76.246.10.212" /*D*/, "143.198.72.88" /*bot*/}
-
-	logger.Info("checkLogin, fullPath: ", c.FullPath())
-	logger.Info("client ip: ", c.ClientIP())
-	logger.Info("client ip is whitelisted: ", contains(ipWhitelist, c.ClientIP()))
 
 	if strings.HasPrefix(c.FullPath(), "/xui/api") && contains(ipWhitelist, c.ClientIP()) {
 		c.Next()
