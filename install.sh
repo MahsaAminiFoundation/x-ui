f04dd4eee7a728ae4116e0f6960578a54272afd5
MahsaAminiVPN <mahsa.nika.hadis.sarina@gmail.com>
2022-11-05 13:11:47 -0700

    update to have english prompts, add crontab installation

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ else
     echo -e "${red}检测架构失败，使用默认架构: ${arch}${plain}"
 fi
 
-echo "架构: ${arch}"
+echo "Architecture: ${arch}"
 
 if [ $(getconf WORD_BIT) != '32' ] && [ $(getconf LONG_BIT) != '64' ]; then
     echo "本软件不支持 32 位系统(x86)，请使用 64 位系统(x86_64)，如果检测有误，请联系作者"
@@ -75,31 +75,36 @@ fi
 
 install_base() {
     if [[ x"${release}" == x"centos" ]]; then
-        yum install wget curl tar -y
+        yum install wget curl tar sqlite3 -y
     else
-        apt install wget curl tar -y
+        apt install wget curl tar sqlite3 -y
     fi
 }
 
 #This function will be called when user installed x-ui out of sercurity
 config_after_install() {
-    echo -e "${yellow}出于安全考虑，安装/更新完成后需要强制修改端口与账户密码${plain}"
-    read -p "确认是否继续?[y/n]": config_confirm
+    echo -e "${yellow}For security reasons, it is necessary to modify the port and account password after the installation/update is completed.${plain}"
+    read -p "Please confirm?[y/n]": config_confirm
     if [[ x"${config_confirm}" == x"y" || x"${config_confirm}" == x"Y" ]]; then
-        read -p "请设置您的账户名:" config_account
-        echo -e "${yellow}您的账户名将设定为:${config_account}${plain}"
-        read -p "请设置您的账户密码:" config_password
-        echo -e "${yellow}您的账户密码将设定为:${config_password}${plain}"
-        read -p "请设置面板访问端口:" config_port
-        echo -e "${yellow}您的面板访问端口将设定为:${config_port}${plain}"
-        echo -e "${yellow}确认设定,设定中${plain}"
+        read -p "Please enter the username you want to use:" config_account
+        echo -e "${yellow}Your username will be:${config_account}${plain}"
+        read -p "Please enter the password you want to use:" config_password
+        echo -e "${yellow}Password will be:${config_password}${plain}"
+        read -p "Please enter the port number:" config_port
+        echo -e "${yellow}Your panel access port number will be:${config_port}${plain}"
+        echo -e "${yellow}Settings confirmed.${plain}"
         /usr/local/x-ui/x-ui setting -username ${config_account} -password ${config_password}
-        echo -e "${yellow}账户密码设定完成${plain}"
+        echo -e "${yellow}Username/Password setting completed${plain}"
         /usr/local/x-ui/x-ui setting -port ${config_port}
-        echo -e "${yellow}面板端口设定完成${plain}"
+        echo -e "${yellow}Panel port setting is completed${plain}"
     else
-        echo -e "${red}已取消,所有设置项均为默认设置,请及时修改${plain}"
+        echo -e "${red}Cancelled, all setting items are default settings, please modify in time${plain}"
     fi
+}
+
+config_cronjob_files() {
+    echo -e "${yellow}Copying cronjob configs to automatically increase bandwidth weekly${plain}"
+    cp crontab/mahsa_amini_vpn /etc/cron.d/
 }
 
 install_x-ui() {
@@ -112,10 +117,10 @@ install_x-ui() {
             echo -e "${red}检测 x-ui 版本失败，可能是超出 Github API 限制，请稍后再试，或手动指定 x-ui 版本安装${plain}"
             exit 1
         fi
-        echo -e "检测到 x-ui 最新版本：${last_version}，开始安装"
+        echo -e "Detected x-ui; Latest version：${last_version}，starting insallation"
         wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz https://github.com/roozbeh/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz
         if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 x-ui 失败，请确保你的服务器能够下载 Github 的文件${plain}"
+            echo -e "${red}Failed to download x-ui, please make sure your server can download Github files${plain}"
             exit 1
         fi
     else
@@ -124,7 +129,7 @@ install_x-ui() {
         echo -e "开始安装 x-ui v$1"
         wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz ${url}
         if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 x-ui v$1 失败，请确保此版本存在${plain}"
+            echo -e "${red}Failed to download x-ui v$1, please make sure this version exists${plain}"
             exit 1
         fi
     fi
@@ -142,6 +147,7 @@ install_x-ui() {
     chmod +x /usr/local/x-ui/x-ui.sh
     chmod +x /usr/bin/x-ui
     config_after_install
+    config_cronjob_files
     #echo -e "如果是全新安装，默认网页端口为 ${green}54321${plain}，用户名和密码默认都是 ${green}admin${plain}"
     #echo -e "请自行确保此端口没有被其他程序占用，${yellow}并且确保 54321 端口已放行${plain}"
     #    echo -e "若想将 54321 修改为其它端口，输入 x-ui 命令进行修改，同样也要确保你修改的端口也是放行的"
@@ -153,20 +159,20 @@ install_x-ui() {
     systemctl start x-ui
     echo -e "${green}x-ui v${last_version}${plain} 安装完成，面板已启动，"
     echo -e ""
-    echo -e "x-ui 管理脚本使用方法: "
+    echo -e "How to use the management script for x-ui: "
     echo -e "----------------------------------------------"
-    echo -e "x-ui              - 显示管理菜单 (功能更多)"
-    echo -e "x-ui start        - 启动 x-ui 面板"
-    echo -e "x-ui stop         - 停止 x-ui 面板"
-    echo -e "x-ui restart      - 重启 x-ui 面板"
+    echo -e "x-ui              - Show admin menu (more functions)"
+    echo -e "x-ui start        - Start x-ui Panel"
+    echo -e "x-ui stop         - Stop x-ui Panel"
+    echo -e "x-ui restart      - Restart x-ui Panel"
     echo -e "x-ui status       - 查看 x-ui 状态"
     echo -e "x-ui enable       - 设置 x-ui 开机自启"
     echo -e "x-ui disable      - 取消 x-ui 开机自启"
     echo -e "x-ui log          - 查看 x-ui 日志"
     echo -e "x-ui v2-ui        - 迁移本机器的 v2-ui 账号数据至 x-ui"
-    echo -e "x-ui update       - 更新 x-ui 面板"
-    echo -e "x-ui install      - 安装 x-ui 面板"
-    echo -e "x-ui uninstall    - 卸载 x-ui 面板"
+    echo -e "x-ui update       - 更新 x-ui Panel"
+    echo -e "x-ui install      - 安装 x-ui Panel"
+    echo -e "x-ui uninstall    - 卸载 x-ui Panel"
     echo -e "----------------------------------------------"
 }
 
