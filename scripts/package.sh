a57ced4b82a2a5c74e0b27eddaf2f56911242856
Roozbeh <mahsa.nika.hadis.sarina@gmail.com>
2022-11-05 22:13:48 -0700

    updated instructions for updating binary pkg

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,3 +1,12 @@
+On dev server:
+$ go build -ldflags "-s -w" main.go 
+
+
+On mac laptop:
+cd /Users/roozbeh/GoogleDrive/iPronto/MahsaAminiVPN/x-ui-binary
+scp ubuntu@140.82.48.210:/home/ubuntu/x-ui/main x-ui/x-ui
+tar czvf x-ui-linux-amd64.tar.gz x-ui/
+
 mkdir x-ui
 cd x-ui
 scp root@<dev-server>:/home/ubuntu/x-ui/main x-ui
